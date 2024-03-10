fecbda352fbd2136087fa7de74d5c1c978164d22
Sam Smith <leonardopacher@gmail.com>
2024-03-10 08:01:31 +0100

    also a test, nothing important

--- scripts/sum-all.py
+++ scripts/sum-all.py
@@ -1,13 +1,19 @@
 import pandas as pd
 from transformers import pipeline
 
-
-df = pd.read_csv('scripts/final_table.csv')
-
-colum_name = df['plot_synopsis']
-
 # Initialize the summarization pipeline with the model
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+
+# Function to filter DataFrame based on search query
+def filter_options(df, search_query, max_suggestions=5):
+    """
+    Creates a suggestions dataframe based on the user's search_query
+
+    Returns:
+        DataFrame
+    """
+    suggestions = df[df['title'].str.contains(search_query, case=False)][:max_suggestions]
+    return suggestions['title'].tolist()
 
 def summarize_column(df, column_name):
     # Function to summarize each row in the specified column
@@ -19,10 +25,11 @@ def summarize_column(df, column_name):
     df['Summarized_' + column_name] = df[column_name].apply(summarize_text)
     return df
 
+# Read the CSV file into a DataFrame
+df = pd.read_csv('scripts/final_table.csv')
 
+# Assuming 'plot_synopsis' is the column you want to summarize
+df = summarize_column(df, 'plot_synopsis')
 
-
-
-# Example usage:
-# Assuming df is your DataFrame and 'synopsis' is the column you want to summarize
-# df = summarize_column(df, 'synopsis')
+# Display the DataFrame with the new column
+print(df)
